554ccb5e9067db39c3fd637fe41ba790be1455ed
havidtech <76119744+havidtech@users.noreply.github.com>
2022-03-16 21:50:01 +0100

    feat: implement boradApeTokenStaker

--- contracts/BoredApeTokenStaker.sol
+++ contracts/BoredApeTokenStaker.sol
@@ -20,41 +20,67 @@ contract BoredApeTokenStaker {
 
     using SafeMath for uint256;
 
-    mapping(address => uint) private stake;
+    mapping(address => uint) private stakes;
     mapping(address => uint) private timeOfStake;
 
-    address private immutable brtAddress;
-    address private immutable baycAddress;
-    uint private immutable roiRate;
-    uint private immutable roiPeriod;
-    uint private immutable minimumVestingPeriod;
+    IBoredApeToken private immutable brtToken;
+    IBoredApeYachtClub private constant baycToken = IBoredApeYachtClub(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
+    uint private constant ROI_FACTOR = 10;
+    uint private constant ROI_PERIOD = 30 days;
+    uint private constant MIN_VESTING_PERIOD = 3 days;
 
-    constructor(address _brtAddress, address _baycAddress, uint _roiRate, uint _roiPeriod, uint _minimumVestingPeriod){
-        brtAddress = _brtAddress;
-        baycAddress = _baycAddress;
-        roiRate = _roiRate;
-        roiPeriod = _roiPeriod;
-        minimumVestingPeriod = _minimumVestingPeriod;
+    constructor(address _brtAddress){
+        brtToken = IBoredApeToken(_brtAddress);
     }
 
-    function stakeToken(uint _value) public returns (bool){
-        // Require that msg.sender should not have a running stake
-        // Require that msg.sender has BAYC NFT
-        // Require sufficient allowance
-        // Require _value to be a multiple of the roiRate
-        // Stake a token
+    function stakeToken(uint toStake) public returns (bool){
+        require(stakes[msg.sender] == 0, "You already have a stake");
+        require(baycToken.balanceOf(msg.sender) > 0, "You must have BAYC NFT");
+        require(toStake % (MIN_VESTING_PERIOD * ROI_FACTOR) == 0, "stake should be divisible by MIN_VESTING PERIOD * ROI_FACTOR");
+        require(brtToken.balanceOf(msg.sender) >= toStake, "Insufficient BRT" );
+        require(brtToken.allowance(msg.sender, address(this)) >= toStake, "You haven't approved us");
+        
+        // Stake token
+        brtToken.transferFrom(msg.sender, address(this), toStake);
+        stakes[msg.sender] = toStake;
+        timeOfStake[msg.sender] = block.timestamp;
+
+        return true;
     }
 
     function withdrawStake() public returns (bool) {
-        // Require this contract has sufficient balance. 
-        // Bankcruptcy can happen here.
-        // Require that msg.sender has some stakes
-        // Reset vesting time.
+        uint _stakes = stakes[msg.sender];
+        require(_stakes > 0, "You have no stake");
+        require(brtToken.balanceOf(address(this)) >= _stakes, "Servie Downtime. Try later" );
+        stakes[msg.sender] = 0; 
+        brtToken.transfer(msg.sender, _stakes + computeROI(msg.sender));
     }
 
     function withdrawProfit() public returns (bool) {
-        // Require minimum vesting time
-        // Reset vesting time.
+        uint _stakes = stakes[msg.sender];
+        require(_stakes > 0, "You have no stake");
+
+        uint roi = computeROI(msg.sender);
+        require(roi > 0, "You have no returns now");
+
+        require(brtToken.balanceOf(address(this)) >= roi, "Servie Downtime. Try later" );
+        timeOfStake[msg.sender] = block.timestamp;
+        brtToken.transfer(msg.sender, roi);
+
+        return true;
+    }
+
+    function computeROI(address staker) internal view returns (uint) {
+        uint daysOfStake = (block.timestamp - timeOfStake[staker]) / 1 days;
+        if(daysOfStake < MIN_VESTING_PERIOD){
+            return 0;
+        }
+
+        return (stakes[staker] / ( ROI_FACTOR * ROI_PERIOD ) ) * daysOfStake;
+    }
+
+    function resetTimeOfStake(address staker) internal {
+        timeOfStake[staker] = block.timestamp;
     }
 
 }
